db06220379a54b18f04ba7d473dfe3ed7a13009b
Hasan Poonawala <hasan.poonawala@uky.edu>
2021-02-09 14:12:02 -0500

    add convenience functions in spatial algebra

--- julia/spatial_algebra/main.jl
+++ julia/spatial_algebra/main.jl
@@ -1,16 +1,39 @@
+function update_planar_state(state,mvis,q1,q2,q3)
+    set_configuration!(state, [q1, q2,q3])
+    set_configuration!(mvis, configuration(state))
+end
 
-# Obtain the end-effector frame and world frame of robot
-ee = bodies(mechanism)[6]
-ee_frame=default_frame(ee)
-world_frame = root_frame(mechanism)
-# Define point
-x = Point3D(ee_frame, 0.2, 0.2, 0.2)
-setelement!(mvis, ee_frame)
-rad=0.05; # radius of point marker sphere
-name = "p1"
-setelement!(mvis,x, rad, name)
+function calcXinW(state,x)
+    mechanism = state.mechanism;
+    ee_frame=default_frame(bodies(mechanism)[end])
+    base_frame = default_frame(bodies(mechanism)[1])
+    # Obtain the transformation from world to EE
+    EEinBase = relative_transform(state,base_frame,ee_frame)
+    x_in_w = inv(EEinBase)*x
+    println("x in world: $((x_in_w).v)")
+    # x_in_ee = EEinBase * x_in_w
+    # println("x in block: $x_in_ee")
 
-# Obtain the transformation from world to EE
-EEinBase = relative_transform(state,world_frame,ee_frame)
-x_in_w = inv(EEinBase)*x
-x_in_ee = EEinBase * x_in_w
+    or_w = Point3D(ee_frame, 0.0, 0.0, 0.0);
+    x1_w = inv(EEinBase)*(Point3D(ee_frame, 1.0, 0.0, 0.0) - or_w)
+    x2_w = inv(EEinBase)*(Point3D(ee_frame, 0.0, 1.0, 0.0) - or_w)
+    x3_w = inv(EEinBase)*(Point3D(ee_frame, 0.0, 0.0, 1.0) - or_w)
+    R = hcat(x1_w.v,x2_w.v,x3_w.v)
+    x_in_w_manual = R *x.v + (inv(EEinBase)*Point3D(ee_frame, 0.0, 0.0, 0.0)).v
+    println("x in world manual: $x_in_w_manual")
+end
+
+# Load mechanism and visualize at some config
+delete!(vis)
+mvis, mechanism = display_urdf("planar3R.urdf",vis)
+state = MechanismState(mechanism)
+update_planar_state(state,mvis,pi/3,pi/3,pi/3)
+
+# Create base and end-effector frames:
+ee_frame=default_frame(bodies(mechanism)[end])
+base_frame = default_frame(bodies(mechanism)[1])
+# Define point in frame of end effector
+x = Point3D(ee_frame, 1.0, 0.0, 0.0)
+# Visualize this point
+setelement!(mvis,x, 0.05, "p1") # mvis, point, radius, name
+calcXinW(state,x)
--- julia/spatial_algebra/test_block.jl
+++ julia/spatial_algebra/test_block.jl
@@ -5,11 +5,31 @@ function update_block_state(state,mvis,theta,w,d)
     set_configuration!(mvis,configuration(state))
 end
 
+function calcXinW(state,x)
+    mechanism = state.mechanism;
+    ee_frame=default_frame(bodies(mechanism)[end])
+    base_frame = default_frame(bodies(mechanism)[1])
+    # Obtain the transformation from world to EE
+    EEinBase = relative_transform(state,base_frame,ee_frame)
+    x_in_w = inv(EEinBase)*x
+    println("x in world: $((x_in_w).v)")
+    # x_in_ee = EEinBase * x_in_w
+    # println("x in block: $x_in_ee")
+
+    # Manual reconstruction:
+    or_w = Point3D(ee_frame, 0.0, 0.0, 0.0);
+    x1_w = inv(EEinBase)*(Point3D(ee_frame, 1.0, 0.0, 0.0) - or_w)
+    x2_w = inv(EEinBase)*(Point3D(ee_frame, 0.0, 1.0, 0.0) - or_w)
+    x3_w = inv(EEinBase)*(Point3D(ee_frame, 0.0, 0.0, 1.0) - or_w)
+    R = hcat(x1_w.v,x2_w.v,x3_w.v)
+    x_in_w_manual = R *x.v + (inv(EEinBase)*Point3D(ee_frame, 0.0, 0.0, 0.0)).v
+    println("x in world manual: $x_in_w_manual")
+end
 delete!(vis)
 mvis, mechanism = display_urdf("block.urdf",vis)
 state = MechanismState(mechanism)
 # frame rotation as axis, angle theta, w/norm(w)
-theta = pi/3;
+theta = 0.0;
 w = [1.0,1.0,1.0];
 # frame origin in world
 d = [0.0,0.0,0.0];
@@ -18,17 +38,11 @@ update_block_state(state,mvis,theta,w,d)
 # Create base and end-effector frames:
 ee_frame=default_frame(bodies(mechanism)[end])
 base_frame = default_frame(bodies(mechanism)[1])
-
-
 # Define point in frame of end effector
 x = Point3D(ee_frame, 1.0, 0.0, 0.0)
 # Visualize this point
 setelement!(mvis,x, 0.05, "p1") # mvis, point, radius, name
 
-# Obtain the transformation from world to EE
-EEinBase = relative_transform(state,base_frame,ee_frame)
-x_in_w = inv(EEinBase)*x
-println("x in world: $x_in_w")
-# x_in_ee = EEinBase * x_in_w
 
-# println("x in block: $x_in_ee")
+#update_block_state(state,mvis,pi/3,w,d)
+#calcXinW(state,x)
