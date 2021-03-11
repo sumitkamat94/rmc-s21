639166d44d24508a44febc49f8896db0c0b040ee
Hasan Poonawala <hasan.poonawala@uky.edu>
2021-03-11 08:11:45 -0500

    updated wam 3 link, inverse dynamics call

--- julia/odes/wam_three_link.jl
+++ julia/odes/wam_three_link.jl
@@ -18,8 +18,21 @@ function move_robot(mvis,mechanism)
     setanimation!(mvis, sol; realtime_rate = 1.0);
 end
 
+function getQdesSV(state,a,b,c)
+    # Creates the SegmentedVector structure using the state
+    qdd_des = similar(velocity(state))
+    qdd_des[1] = a;
+    qdd_des[2] = b;
+    qdd_des[3] = c;
+    return qdd_des
+end
 ## to restart visualization:
 delete!(vis)
 mvis, mechanism = display_urdf("three_link_assn2.urdf",vis)
-
-move_robot(mvis,mechanism)
+state = MechanismState(mechanism)
+set_configuration!(state,[0.5,0.5,0.5])
+set_configuration!(mvis, configuration(state))
+set_velocity!(state,[1.0,1.0,1.0])
+qdd = getQdesSV(state,2.0,2.0,2.0)
+inverse_dynamics(state,qdd)
+# move_robot(mvis,mechanism)
